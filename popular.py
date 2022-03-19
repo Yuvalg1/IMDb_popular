7d9457c4ad5abf4777a4a99f8f995d33f9c5034e
Yuvalg1 <yuvalgoldvarg@gmail.com>
2022-03-19 15:43:09 +0200

    try catch to exception, printed the html code of the page

--- popular.py
+++ popular.py
@@ -1,6 +1,11 @@
 from bs4 import BeautifulSoup
 import requests
 
-source = requests.get('https://www.imdb.com/chart/moviemeter/?ref_=nv_mv_mpm') # get most popular movies site
+try:
+    source = requests.get('https://www.imdb.com/chart/moviemeter/?ref_=nv_mv_mpm') # get most popular movies site
+    source.raise_for_status() # throw an exception if address is invalid
 
-source.raise_for_status() # throw an exception if address is invalid
+    soup = BeautifulSoup(source.text, 'html.parser')
+    print(soup)
+except Exception as e:
+    print(e)
